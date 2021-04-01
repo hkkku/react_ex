0b97c705246a754096e095682e68e0e44ec8f86a
hkkku <hhk7045@gmail.com>
2021-04-01 18:20:43 +0900

    04/01 react 세팅

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { Component } from "react";
+
+class Greeting extends Component {
+  // render 함수 내부에 return 값은 반드시 하나의 태그로 감싸져야 한다.
+  render() {
+    return (
+      <div>
+        <p>Hello Again React</p>
+      </div>
+    )
+  }
+}
 function App() {
   return (
     <div className="App">
       <p>Hello React</p>
+      <Greeting />
     </div>
   );
 }
